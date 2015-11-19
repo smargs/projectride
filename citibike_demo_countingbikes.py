b56230642d1206a771328fa4d3c247caeb26fffd
smargs <pushkarini.a@gmail.com>
2015-11-19 13:13:46 -0800

    Update citibike_demo_countingbikes.py

--- citibike_demo_countingbikes.py
+++ citibike_demo_countingbikes.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-filename = '/Users/Pushkarini/Desktop/dataincubator_work/data/b1509.csv'
+filename = '/data/b1509.csv'
 
 data = pd.read_csv(filename) 
 
